1cb86f3430d267aa13918e9916528f3c45823f99
TheMaverickProgrammer <91709+TheMaverickProgrammer@users.noreply.github.com>
2026-02-21 21:25:08 -0600

    minor fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
             name='python-markdown-secrets',
-            install_requires=['markdown?=3'],
+            install_requires=['markdown>=3'],
             tests_require=['mkdocs'],
             py_modules=['mkdsecrets']
             )
